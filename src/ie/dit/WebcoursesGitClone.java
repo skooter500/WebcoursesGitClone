154bb63f8fbbe412f59c230d01bab29c6a04710e
unknown <skooter500>
2015-02-09 11:25:12 +0000

    Bla

--- src/ie/dit/WebcoursesGitClone.java
+++ src/ie/dit/WebcoursesGitClone.java
@@ -1,30 +1,19 @@
 package ie.dit;
 
 
-
+// What does import mean?
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.rowset.WebRowSet;
 
 public class WebcoursesGitClone 
 {
 	private String src;
 	private String dest;
 	
+	// What is this an example of?
 	public WebcoursesGitClone(String src, String dest)
 	{
 		this.src = src;
@@ -39,28 +28,45 @@ public class WebcoursesGitClone
 			return;
 		}
 		
-		WebcoursesGitClone wgc = new WebcoursesGitClone(args[0], args[1]);
-		
+		// Where do the values come from for the args array?
+		WebcoursesGitClone wgc = new WebcoursesGitClone(args[0], args[1]);		
 		wgc.doWork();
 		
 	}
 
 	private void doWork()
 	{
-		File[] files = new File(src).listFiles(new FileFilter()
+		
+		// What does the File class in Java do?
+		// What package is it in?
+		// What is an anonymous inner class?
+		File[] files = new File(src).listFiles(new FileFilter() // What does the FileFilter class do?
 		{
+			// What object is this method on?
 			public boolean accept(File f)
 			{
+				// toLowerCase and endsWith are methods on the String class
+				// What parameters do they take and what do they return?				
 				return (f.getName().toLowerCase().endsWith(".txt"));
+				
+				// How do you do the following to a String:
+				// - Extract a substring
+				// - Find a String in a String
+				// Replace a String in a String
+				// What is a StringBuilder and why is it useful?
 			}
 		});
-		if (files == null)
+		
+		// Why would this ever happen? WHy do we need to check for null?
+		if (files == null) // What does null mean?
 		{
 			System.out.println("No files found");
 			return;
 		}
+		// What does the : mean in this for loop?
 		for (File file : files) 
 		{
+			// Why would the following code ever return false?
 		    if (file.isFile()) 
 		    {
 		        processFile(file);
@@ -73,24 +79,41 @@ public class WebcoursesGitClone
 		String studentNumber = "";
 		String studentName = "";
 		String url = "";
+		
+		// What does try mean?
 		try
 		{
+			// What does indexOf do?
 			int start_ = file.getName().indexOf("_");
+			// Why is the second parameter start_ + 1?
 			int end_ = file.getName().indexOf("_", start_ + 1);
+			
+			// Why is the first parameter start_ + 1?
 			studentNumber = file.getName().substring(start_ + 1, end_);
 			String current;
 			
+			// What is a BufferedReader used for?
+			// Why do we pass a FileReader as a parameter?
+			// Why do we pass a File object as a parameter to the FileReader?
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			
+			// What does this line do??
+			// WHat does the readLIne method return?
 			while ((current = reader.readLine()) != null) 
 			{
+				// What does contains return and what does it do?
 				if (current.contains("Name: "))
 				{
 					int i = current.indexOf("(");
+					// What do the parameters to substring do? 
+					// What happens if you passed a negative number or 
+					// A number greater than the number of characters in the STring as parameters
 					studentName = current.substring(6, i - 1);
 				}
 				if (current.contains("http"))
 				{
+					// What the hell does the parameter to replaceAll mean??
+					// I want a detailed answer!
 					current = current.replaceAll("\\<[^>]*>","");
 					int i = current.indexOf("http");
 					url = current.substring(i, current.length());
@@ -99,32 +122,42 @@ public class WebcoursesGitClone
 					break;
 				}
 			}
-			reader.close();
+			reader.close(); // Why do we do this?
 			studentNumber = "";
 			studentName = "";
 			url = "";
 		} 
-		catch (Exception e)
-		{
+		// What does try/catch mean?
+		// Whats the significance of catching the Exception type here?
+		catch (Exception e) 
+		{			
 			System.out.println("Error cloning:  " + url + " Student: " + studentNumber + " Name: " + studentName);
-			// TODO Auto-generated catch block
+			// What will this line do?
 			e.printStackTrace();
-		}
-		
+		}		
 	}
 
+	
 	private void cloneRepo(String studentNumber, String studentName, String url)
 	{
+		// WHat do these next two lines do?
 		int i = url.lastIndexOf("/");
 		String projectName = url.substring(i + 1, url.length());
-		String destPath = dest + "\\" + studentNumber + " " + studentName + "\\" + projectName;
+		
+		// What will destPath look like after this line executes		
+		String destPath = dest + File.separator + studentNumber + " " + studentName + File.separator + projectName;
+		// What is the type File.separator?
+		// What will its value on Windows/Linux/Android/MacOS be?
+		
+		// What is happening here?
 		String[] cmd = {
 				"git"
 				, "clone"
 				, url
 				, destPath
 		};			
-		
+
+		// What do the next few lines mean?
 		File test = new File(destPath);
 		if (test.exists())
 		{
@@ -134,11 +167,18 @@ public class WebcoursesGitClone
 		
 		try
 		{
+			// What is a Process object?
+			// What package is Process in?
 			Process process;
+			
+			// What does this line do?
+			// What is the second parameter? Why is it null?
 			process = Runtime.getRuntime().exec(cmd,null);
 			
+			// What is an InputStream?
 			InputStream in = process.getErrorStream();
 	        int c;
+	        // What does in.read do?
 	        while ((c = in.read()) != -1) {
 	            System.out.print((char)c);
 	        }
@@ -155,6 +195,8 @@ public class WebcoursesGitClone
 			*/
 			while(process.isAlive())
 			{
+				// What type of exception can this throw?
+				// What does the code do?
 				Thread.sleep(1000);
 			}
 			
